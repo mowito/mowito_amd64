fb785ba5db8f3567a4817629505770486502820f
puru07 <puru@mowito.in>
2020-06-14 13:48:30 +0530

    merge files from melodic and changes to setup

--- setup_mowito.sh
+++ setup_mowito.sh
@@ -7,19 +7,40 @@ if [ $# -eq 0 ]
     ros_version=kinetic
 fi
 
+
+# create the mwoito dir
+CURR_DIR="$(dirname $(realpath $0))"
+
+if [ -d "$HOME/mowito" ] 
+then
+    echo "Directory ~/mowito exists." 
+else
+    echo "creating ~/mowito directory."
+    mkdir ~/mowito
+fi
+
+FINAL_DIR=$HOME/mowito
+echo "$CURR_DIR"
+echo "$FINAL_DIR"
+
+if [ $FINAL_DIR = $CURR_DIR ]
+then 
+    echo "Already were in Mowito directory"
+else 
+    echo "Copying the data from $CURR_DIR to $FINAL_DIR"
+    cp -r "$CURR_DIR"/. "$FINAL_DIR"
+fi
+
 echo "===================================="
 echo "Installing Mowito packages"
 echo "===================================="
 
-# get the address of the directory
-BASEDIR=$(dirname "$0")
-# echo "$BASEDIR"
 # installing the dependdencies
 sudo apt install ros-${ros_version}-voxel-grid -y
 sudo apt install ros-${ros_version}-openslam-gmapping -y
 sudo apt install ros-${ros_version}-voxel-grid -y
 
-sudo dpkg -i $BASEDIR/debians/ros-${ros_version}-*.deb
+sudo dpkg -i ~/mowito/debians/ros-${ros_version}-*.deb
 
 # registering the user
 echo ""
@@ -29,3 +50,14 @@ echo "=============================="
 source /opt/ros/${ros_version}/setup.bash
 rosrun mlicense robot_reg.py
 
+
+if [ $FINAL_DIR = $CURR_DIR ]
+then 
+    echo "Already were in Mowito directory"
+elif [ $CURR_DIR = $HOME ]
+then
+    echo "$CURR_DIR is the home directory"
+else 
+    echo "deleting the old directory of $CURR_DIR"
+    sudo rm -r  "$CURR_DIR"
+fi
