d0bb97f6cb1444029902fa9e92725ab459afc79e
varshmail7 <varshmail7@gmail.com>
2020-12-29 22:26:36 +0530

    install dependencies via same script

--- setup_mowito.sh
+++ setup_mowito.sh
@@ -1,55 +1,43 @@
 #!/bin/bash
 
 ros_version=$1
+skip_dependencies=$2
+
 if [ $# -eq 0 ]
   then
     echo "defualting the ros version to melodic"
     ros_version=melodic
 fi
 
+#optional arguement to skip installing dependencies
+if [ -z $2 ]
+  then
+    skip_dependencies=False
+else
+	skip_dependencies=True
+fi
 
 # create the mwoito dir
 CURR_DIR="$(dirname $(realpath $0))"
 
 echo "$CURR_DIR"
 
+arch_version="${CURR_DIR: -5}"
 
-echo "===================================="
-echo "Installing Dependencies"
-echo "===================================="
+#install dependencies and mowito packages
+python3 ${CURR_DIR}/install_scripts/install_mowito.py ${ros_version} ${arch_version} ${skip_dependencies}
 
+if [ $? -ne 1 ]
+then
+	# registering the user
+	echo ""
+	echo "=============================="
+	echo "==== ROBOT REGISTRATION ======"
+	echo "=============================="
+	source /opt/ros/${ros_version}/setup.bash
+	rosrun mlicense robot_reg.py
 
-# installing the dependdencies
-sudo apt install ros-${ros_version}-voxel-grid -y
-sudo apt install ros-${ros_version}-openslam-gmapping -y
-sudo apt install ros-${ros_version}-voxel-grid -y
-sudo apt install ros-${ros_version}-joint-state-publisher -y
-sudo apt install ros-${ros_version}-joint-state-controller -y
-
-sudo apt-get install ros-${ros_version}-behaviortree-cpp-v3 -y      # installation of behavior trees for teleop keyboard
-sudo apt-get install ros-${ros_version}-teleop-twist-keyboard -y    # installation of teleop keyboard
-
-sudo apt-get install ros-${ros_version}-map-server                  # installation of map server 
-
-echo "===================================="
-echo "Installing Mowito packages"
-echo "===================================="
-
-
-# sudo dpkg -i $CURR_DIR/debians/ros-${ros_version}-*.deb
-
-python3 ${CURR_DIR}/install_scripts/install_mowito.py ${ros_version} amd64
-
-
-
-# registering the user
-echo ""
-echo "=============================="
-echo "==== ROBOT REGISTRATION ======"
-echo "=============================="
-source /opt/ros/${ros_version}/setup.bash
-rosrun mlicense robot_reg.py
-
-echo "=============================="
-echo "===== ROBOT SETUP DONE ======="
-echo "=============================="
+	echo "=============================="
+	echo "===== ROBOT SETUP DONE ======="
+	echo "=============================="
+fi
