402e04a719be8cd717cb932223b0bcdfdd1c053a
puru rastogi <puru@mowito.in>
2020-12-31 03:14:04 +0530

    update the install scripts

--- README.md
+++ README.md
@@ -8,3 +8,26 @@
 
 - [How to use](https://mowito-navstack.readthedocs.io/en/latest/how_to_use.html)
 
+## Instructions for running setup_mowito.sh
+
+### Usage:
+
+`setup_mowito.sh`  
+
+`setup_mowito.sh -h`
+
+`setup_mowito.sh [rosversion]`
+
+`setup_mowito.sh [rosversion] [arch-version] skip-depends`
+
+### Arguments
+- `-h`          : Help, display usage
+- `rosversion`   : Version of ROS installed(noetic/melodic). Defaults to melodic if not specified
+- `arch-version` : Archtecture to be installed (amd64/arm64). Defaults to amd64 if not specified
+- `skip-depends` : Skip installing dependencies (optional). Installs dependencies if not specified
+
+### Example
+```
+./setup_mowito.sh melodic amd64 
+```
+Installs noetic amd64 debians 
--- setup_mowito.sh
+++ setup_mowito.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
 ros_version=$1
-skip_dependencies=$2
+arch_version=$2
+skip_dependencies=$3
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: setup_mowito.sh"
+  echo "Usage: setup_mowito.sh [-h]"
+  echo "Usage: setup_mowito.sh [rosversion]"
+  echo "Usage: setup_mowito.sh [rosversion] [arch-version]"
+  echo "Usage: setup_mowito.sh [rosversion] [arch-version] skip-depends"
+  echo "Positional arguements"
+  echo "	-h           : Help, display usage"
+  echo "	rosversion   : Version of ROS installed(noetic/melodic)"
+  echo "		       Defaults to melodic if not specified"
+  echo "	arch-version : Archtecture to be installed (amd64/arm64)"
+  echo "		       Defaults to amd64 if not specified"
+  echo "	skip-depends : Skip installing dependencies (optional)"
+  echo "	               Installs dependencies if not specified"
+  exit 0
+fi
+
 
 if [ $# -eq 0 ]
   then
-    echo "defualting the ros version to melodic"
+    echo "defaulting the ros version to melodic and arch to amd64"
     ros_version=melodic
+    arch_version=amd64
+fi
+
+if [ $# -eq 1 ]
+  then
+    echo "defaulting arch to amd64"
+    arch_version=amd64
 fi
 
 #optional arguement to skip installing dependencies
-if [ -z $2 ]
+if [ -z $3 ]
   then
     skip_dependencies=False
 else
@@ -22,7 +48,7 @@ CURR_DIR="$(dirname $(realpath $0))"
 
 echo "$CURR_DIR"
 
-arch_version="${CURR_DIR: -5}"
+sudo echo ""
 
 #install dependencies and mowito packages
 python3 ${CURR_DIR}/install_scripts/install_mowito.py ${ros_version} ${arch_version} ${skip_dependencies}
