2e2fd820813f114b69d0c06fdd54c0cca3b28be6
Blebot0 <keshav29kapur@gmail.com>
2021-05-08 15:52:29 +0530

    Changed the size of navigation bar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,15 +89,13 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #
 # html_theme_options = {}
 html_theme_options = {
-    'logo_only': False,
-    'navigation_depth': 5,
 }
 html_logo = "Images/logo/mowito_icon.png"
 html_favicon = "Images/logo/mowito_icon.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
